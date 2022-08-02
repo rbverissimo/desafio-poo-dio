1bb09e133b3d884d16055dbf207c963cb55521fa
rbverissimo <rverissimo.dev@gmail.com>
2022-08-02 11:21:13 -0300

    updates na classe Dev

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -1,6 +1,7 @@
 package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -16,9 +17,18 @@ public class Dev {
 		this.nome = nome;
 	}
 	
-	public void inscreverBootcamp(Bootcamp bootcamp){};
+	public void inscreverBootcamp(Bootcamp bootcamp){
+		this.conteudosInscritos.addAll(bootcamp.getConteudos());
+		bootcamp.getDevsInscritos().add(this);
+	};
 	
-	public void progredir(){};
+	public void progredir(){
+		Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst(); 
+		if(conteudo.isPresent()) { 
+			this.conteudosConcluidos.add(conteudo.get());
+			this.conteudosInscritos.remove(conteudo.get()); 
+		} else System.err.println("Você não está matriculado em nenhum conteúdo");
+	};
 	
 	public void calcularTotalXp(){}
 	
