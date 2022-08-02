7dda17470d5d76376db27fffb2057ac2822018d6
rbverissimo <rverissimo.dev@gmail.com>
2022-08-01 23:50:46 -0300

    adicionando as classes Dev e Bootcamp modeladas

---
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -0,0 +1,105 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Bootcamp {
+	
+	private String nome;
+	private String descricao; 
+	private LocalDate dataInicial = LocalDate.now();
+	private final LocalDate DATA_FINAL = dataInicial.plusDays(45);
+	private Set<Dev> devsInscritos = new HashSet<>();
+	private Set<Conteudo> conteudos = new LinkedHashSet<>();
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public LocalDate getDataInicial() {
+		return dataInicial;
+	}
+	public void setDataInicial(LocalDate dataInicial) {
+		this.dataInicial = dataInicial;
+	}
+	public Set<Dev> getDevsInscritos() {
+		return devsInscritos;
+	}
+	public void setDevsInscritos(Set<Dev> devsInscritos) {
+		this.devsInscritos = devsInscritos;
+	}
+	public Set<Conteudo> getConteudos() {
+		return conteudos;
+	}
+	public void setConteudos(Set<Conteudo> conteudos) {
+		this.conteudos = conteudos;
+	}
+	public LocalDate getDATA_FINAL() {
+		return DATA_FINAL;
+	}
+	
+	
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((DATA_FINAL == null) ? 0 : DATA_FINAL.hashCode());
+		result = prime * result + ((conteudos == null) ? 0 : conteudos.hashCode());
+		result = prime * result + ((dataInicial == null) ? 0 : dataInicial.hashCode());
+		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + ((devsInscritos == null) ? 0 : devsInscritos.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+	
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bootcamp other = (Bootcamp) obj;
+		if (DATA_FINAL == null) {
+			if (other.DATA_FINAL != null)
+				return false;
+		} else if (!DATA_FINAL.equals(other.DATA_FINAL))
+			return false;
+		if (conteudos == null) {
+			if (other.conteudos != null)
+				return false;
+		} else if (!conteudos.equals(other.conteudos))
+			return false;
+		if (dataInicial == null) {
+			if (other.dataInicial != null)
+				return false;
+		} else if (!dataInicial.equals(other.dataInicial))
+			return false;
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		if (devsInscritos == null) {
+			if (other.devsInscritos != null)
+				return false;
+		} else if (!devsInscritos.equals(other.devsInscritos))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+	
+}
